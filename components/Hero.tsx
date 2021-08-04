a6c2a400daf927f85b5a6e4ab65703d84f8c1387
Raadwan Masum <piraadwan@gmail.com>
2021-08-03 17:51:18 -0700

    remove stuff from old hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,11 +4,7 @@ const Hero = () => {
 	return (
 		<div className={style.mainbox}>
 			<div className={style.splitboxes}>
-				{/* <div className={style.leftbox}>
-					<img src="/mountains2.png" />
-				</div> */}
 				<div className={style.rightbox}>
-					{/* <img src="/full_logo_transparentbg.png" /> */}
 					<p className={style.bigwords}>
 						<span className={style.tech}>Tech.</span><br></br>
 						<span className={style.business}>Business.</span><br></br>
@@ -26,14 +22,6 @@ const Hero = () => {
 						</span>
 						{' '}mentorship.
 					</p>
-					{/* <p>
-						Connecting students to{' '}
-						<span className={style.color}>
-							transformative
-							<br /> experiences
-						</span>{' '}
-						and <span className={style.color}>new fields of study</span>
-					.</p> */}
 					<button>Learn More</button>
 				</div>
 			</div>
