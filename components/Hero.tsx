2e808be26ac858445a21c970fe5b98bc9135b578
Raadwan Masum <piraadwan@gmail.com>
2021-08-03 18:03:58 -0700

    add zipline man and get him in position

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,23 +4,20 @@ const Hero = () => {
 	return (
 		<div className={style.mainbox}>
 			<div className={style.splitboxes}>
+				<div className={style.leftbox}>
+					<img src="./ziplineman.png" alt="zipline" />
+				</div>
 				<div className={style.rightbox}>
 					<p className={style.bigwords}>
-						<span className={style.tech}>Tech.</span><br></br>
-						<span className={style.business}>Business.</span><br></br>
+						<span className={style.tech}>Tech.</span>
+						<br></br>
+						<span className={style.business}>Business.</span>
+						<br></br>
 						<span className={style.creativity}>Creativity.</span>
 					</p>
 					<p className={style.message}>
-						We connect students to{' '}
-						<span className={style.color}>
-							technology
-						</span>
-						<br></br>
-						{' '}and{' '}
-						<span className={style.color}>
-							business
-						</span>
-						{' '}mentorship.
+						We connect students to <span className={style.color}>technology</span>
+						<br></br> and <span className={style.color}>business</span> mentorship.
 					</p>
 					<button>Learn More</button>
 				</div>
