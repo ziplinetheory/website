8dd037691ccdd1a24a51de1390c2c10ee8d4ee1c
Raadwan Masum <piraadwan@gmail.com>
2021-08-12 17:21:44 -0700

    new navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,17 +3,30 @@ import style from '../styles/Nav.module.scss'
 const Nav = () => {
 	return (
 		<div className={style.navbox}>
-			{/* put logo here */}
-			<a href="/">
-				<img className={style.navlogo} src="./short_logo_whitebg.PNG" alt="zipline logo" />
-			</a>
 			<div className={style.nav}>
+				<div className={style.navlogos}>
+					<a href="/">
+						<img
+							className={style.navlogosmall}
+							src="./short_logo_whitebg.PNG"
+							alt="zipline logo"
+						/>
+					</a>
+					<a href="/">
+						<img
+							className={style.navlogolarge}
+							src="./full_logo_transparentbg.png"
+							alt="zipline logo"
+						/>
+					</a>
+				</div>
 				<div className={style.navlinks}>
 					<ul>
 						<li>
-							<a href="/">
-								Team
-							</a>
+							<a href="/" className={style.active}>Home</a>
+						</li>
+						<li>
+							<a href="/">Team</a>
 						</li>
 						<li>
 							<a href="/">Events</a>
@@ -22,6 +35,9 @@ const Nav = () => {
 							<a href="/">Donate</a>
 						</li>
 					</ul>
+				</div>
+				<div className={style.navbutton}>
+					<button>Contact</button>
 				</div>
 			</div>
 		</div>
