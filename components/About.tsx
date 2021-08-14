e0e500c898fa92b0bfd255c35a5548632468e28d
Raadwan Masum <piraadwan@gmail.com>
2021-08-14 15:04:32 -0700

    add about content

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,20 @@ import style from '../styles/About.module.scss'
 const About = () => {
 	return (
 		<div className={style.mainbox}>
-            <p>About</p>
+			<div className={style.content}>
+				<h1><span>.</span>About Us<span>.</span></h1>
+					<img
+						className={style.aboutgraphic}
+						src="./aboutgraphic.png"
+						alt="about graphic"
+					/>
+				<div className={style.theory}>
+					<p>
+						<span>Our Theory:</span> Students <span>learn best</span> when taught <br />{' '}
+						subjects they are <span>genuinely interested in</span>.
+					</p>
+				</div>
+			</div>
 		</div>
 	)
 }
