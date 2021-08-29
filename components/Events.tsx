827a0984113a2119a2a83e8954d1d7cba24d77e0
Raadwan Masum <piraadwan@gmail.com>
2021-08-29 16:18:23 -0700

    create events page

---
+++ components/Events.tsx
@@ -0,0 +1,10 @@
+import style from '../styles/Events.module.scss'
+
+const Events = () => {
+	return (
+		<div id="about" className={style.mainbox}>
+		</div>
+	)
+}
+
+export default Events
---
+++ pages/events.tsx
@@ -0,0 +1,13 @@
+import Nav from '../components/Nav'
+import Events from '../components/Events'
+
+const EventsPage = () => {
+	return (
+		<>
+			<Nav />
+			<Events />
+		</>
+	)
+}
+
+export default EventsPage
