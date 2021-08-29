22c33d16c556ed926542c77f49db6b1bc9e86745
Raadwan Masum <piraadwan@gmail.com>
2021-08-29 16:31:18 -0700

    add rounded game graphic

--- components/Events.tsx
+++ components/Events.tsx
@@ -4,7 +4,13 @@ const Events = () => {
 	return (
 		<div className={style.mainbox}>
 			<div className={style.topcontent}>
-				<div className={style.leftbox}></div>
+				<div className={style.leftbox}>
+					<img
+						className={style.gamegraphic}
+						src="./gamegraphic_rounded_top.png"
+						alt="game graphic"
+					/>
+                </div>
 				<div className={style.rightbox}></div>
 			</div>
 		</div>
