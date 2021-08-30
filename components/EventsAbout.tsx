b3f42183de4355943d491a7a1301554a569e9e14
Raadwan Masum <piraadwan@gmail.com>
2021-08-29 17:17:08 -0700

    temporary events about pictures

--- components/EventsAbout.tsx
+++ components/EventsAbout.tsx
@@ -4,6 +4,16 @@ const EventsAbout = () => {
 	return (
 		<div className={style.mainbox}>
 			<h1>About the Event</h1>
+			<img
+				className={style.eventsaboutbackground}
+				src="./events_about_background.png"
+				alt="events about background"
+			/>
+			<img
+				className={style.eventsaboutcontent}
+				src="./events_about_content.png"
+				alt="events about content"
+			/>
 		</div>
 	)
 }
