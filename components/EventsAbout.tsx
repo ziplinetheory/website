4fa1d02c5a5e651ece47eb7c6fa1016311c8cb61
Raadwan Masum <piraadwan@gmail.com>
2021-08-29 17:49:53 -0700

    wrap events about content in a content div

--- components/EventsAbout.tsx
+++ components/EventsAbout.tsx
@@ -3,17 +3,19 @@ import style from '../styles/EventsAbout.module.scss'
 const EventsAbout = () => {
 	return (
 		<div className={style.mainbox}>
-			<h1>About the Event</h1>
-			<img
-				className={style.eventsaboutbackground}
-				src="./events_about_background.png"
-				alt="events about background"
-			/>
-			<img
-				className={style.eventsaboutcontent}
-				src="./events_about_content.png"
-				alt="events about content"
-			/>
+			<div className={style.content}>
+				<h1>About the Event</h1>
+				<img
+					className={style.eventsaboutbackground}
+					src="./events_about_background.png"
+					alt="events about background"
+				/>
+				<img
+					className={style.eventsaboutcontent}
+					src="./events_about_content.png"
+					alt="events about content"
+				/>
+			</div>
 		</div>
 	)
 }
