60fa25f571ec3cc0bc00b98214a64f6b15bd4040
Raadwan Masum <piraadwan@gmail.com>
2021-08-29 18:04:38 -0700

    fix "security issue"

--- components/EventsHero.tsx
+++ components/EventsHero.tsx
@@ -37,7 +37,7 @@ const EventsHero = () => {
 						</button>
 					</div>
 
-					<a href="http://gamedev.ziplinetheory.com/"  target="_blank">
+					<a href="http://gamedev.ziplinetheory.com/"  target="_blank" rel="noreferrer">
 						<button>Sign Up Today</button>
 					</a>
 				</div>
