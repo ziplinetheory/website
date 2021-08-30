1bb7cca7a1b989ed52309fae0653eebeca556f8f
Raadwan Masum <piraadwan@gmail.com>
2021-08-29 17:08:47 -0700

    add and style about title

--- components/EventsAbout.tsx
+++ components/EventsAbout.tsx
@@ -3,33 +3,7 @@ import style from '../styles/EventsAbout.module.scss'
 const EventsAbout = () => {
 	return (
 		<div className={style.mainbox}>
-			<div className={style.topcontent}>
-				<div className={style.leftbox}>
-					<img
-						className={style.gamegraphic}
-						src="./gamegraphic_rounded_top.png"
-						alt="game graphic"
-					/>
-				</div>
-				<div className={style.rightbox}>
-					<p>— OCT. 2021</p>
-					<p>
-						Come Experience Zipline Theory’s First Ever <span>Game Design Event</span>
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec
-						tristique ante, ac egestas sem. Praesent euismod vulputate odio, at commodo
-						urna hendrerit ac.
-					</p>
-
-					<div className={style.rightboxbuttons}>
-						<button>January 1st, 2000</button>
-						<button>Lorem Ipsum Dolor</button>
-					</div>
-
-					<button>Sign Up Today</button>
-				</div>
-			</div>
+			<h1>About the Event</h1>
 		</div>
 	)
 }
