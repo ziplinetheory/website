3dc1f2edf61ba58242413fc59f4cd629d6b2d1e1
Raadwan Masum <piraadwan@gmail.com>
2021-09-13 01:21:00 -0700

    create comingsoon component

---
+++ components/ComingSoon.tsx
@@ -0,0 +1,11 @@
+import style from '../styles/ComingSoon.module.scss'
+
+const ComingSoon = () => {
+	return (
+		<div id="coming-soon" className={style.comingsoonbox}>
+            <h1>Coming Soon!</h1>
+		</div>
+	)
+}
+
+export default ComingSoon
