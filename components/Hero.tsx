f51d08b6e2554a46a044cc53a75b3586d946776a
Aadit Gupta <spacegiant21@gmail.com>
2021-09-15 18:18:12 -0700

    add funded by page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,26 +6,64 @@ const scrollDown = () => {
 
 const Hero = () => {
 	return (
-		<div className={style.mainbox}>
-			<div className={style.splitboxes}>
-				<div className={style.leftbox}>
-					<h1>
-						<span>Ziplining</span>
-						<br /> Towards a<br /> Better Future
-					</h1>
-					<p>
-						We connect students to <span>technology</span>
-						<br />
-						and <span>business</span> knowledge.
-					</p>
-					<a href="/events"><button>Upcoming Events</button></a>
-					<button onClick={scrollDown}>Learn More</button>
-				</div>
-				<div className={style.rightbox}>
-					<img className={style.navlogosmall} src="./mountains.gif" alt="mountains" />
+		<>
+			<div className={style.mainbox}>
+				<div className={style.splitboxes}>
+					<div className={style.leftbox}>
+						<h1>
+							<span>Ziplining</span>
+							<br /> Towards a<br /> Better Future
+						</h1>
+						<p>
+							We connect students to <span>technology</span>
+							<br />
+							and <span>business</span> knowledge.
+						</p>
+						<a href="/events">
+							<button>Upcoming Events</button>
+						</a>
+						<button onClick={scrollDown}>Learn More</button>
+					</div>
+					<div className={style.rightbox}>
+						<img className={style.navlogosmall} src="./mountains.gif" alt="mountains" />
+					</div>
 				</div>
 			</div>
-		</div>
+			<div className={style.logobanner}>
+				<div className={style.container}>
+					<div className={style.label}>we have been funded by</div>
+					<div className={style.logogrid}>
+						<a href="https://www.defense.gov/" target="_blank">
+							<img src="./sponsors/defense.png" />
+						</a>
+						<a href="https://northropgrumman.com" target="_blank">
+							<img src="./sponsors/northrop.svg" />
+						</a>
+						<a href="https://apple.com" target="_blank">
+							<img className={style.fixlogo} src="./sponsors/apple.png" />
+						</a>
+						<a href="https://google.com" target="_blank">
+							<img src="./sponsors/google.jpg" />
+						</a>
+						<a href="https://walmart.com" target="_blank">
+							<img src="./sponsors/walmart.jpg" />
+						</a>
+						<a href="https://amazon.com" target="_blank">
+							<img src="./sponsors/amazon.png" />
+						</a>
+						<a href="https://www.aspirations.org/" target="_blank">
+							<img src="./sponsors/asp.png" />
+						</a>
+						<a href="https://www.cognizant.com/" target="_blank">
+							<img src="./sponsors/cognizant.png" />
+						</a>
+						<a href="https://www.ziplinetheory.com/" target="_blank">
+							<img className={style.fixlogo} src="./sponsors/televisia.png" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
 	)
 }
 
