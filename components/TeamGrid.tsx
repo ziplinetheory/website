d12bc1bec01fc0a461205bd1470d4ab968263083
Raadwan Masum <piraadwan@gmail.com>
2021-11-29 18:36:56 -0800

    create team grid

---
+++ components/TeamGrid.tsx
@@ -0,0 +1,63 @@
+import style from '../styles/TeamGrid.module.scss'
+
+const TeamGrid = () => {
+	return (
+		<div id="teamgrid" className={style.mainbox}>
+			<div className={style.content}>
+				<div className={style.grid}>
+					<div className={style.card}>
+						<img
+							className={style.profilepic}
+							src="./teampics/IMG_8584.jpg"
+							alt="team member"
+						/>
+					</div>
+					<div className={style.card}>
+						<img
+							className={style.profilepic}
+							src="./teampics/IMG_8664.jpg"
+							alt="team member"
+						/>
+                    </div>
+					<div className={style.card}>
+						<img
+							className={style.profilepic}
+							src="./teampics/IMG_8625.jpg"
+							alt="team member"
+						/>
+                    </div>
+					<div className={style.card}>
+						<img
+							className={style.profilepic}
+							src="./teampics/IMG_8692.jpg"
+							alt="team member"
+						/>
+                    </div>
+					<div className={style.card}>
+						<img
+							className={style.profilepic}
+							src="./teampics/IMG_8766.jpg"
+							alt="team member"
+						/>
+                    </div>
+					<div className={style.card}>
+						<img
+							className={style.profilepic}
+							src="./teampics/IMG_8772.jpg"
+							alt="team member"
+						/>
+                    </div>
+					<div className={style.card}>
+						<img
+							className={style.profilepic}
+							src="./teampics/IMG_8710.jpg"
+							alt="team member"
+						/>
+                    </div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default TeamGrid
