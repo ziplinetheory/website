4ac30a8684beb4f8259c3af3bd41109a39ca34b3
Raadwan Masum <piraadwan@gmail.com>
2021-11-29 18:40:09 -0800

    add and style full team pic

--- components/TeamGrid.tsx
+++ components/TeamGrid.tsx
@@ -4,6 +4,11 @@ const TeamGrid = () => {
 	return (
 		<div id="teamgrid" className={style.mainbox}>
 			<div className={style.content}>
+				<img
+					className={style.teampic}
+					src="./teampics/IMG_8795.jpg"
+					alt="team member"
+				/>
 				<div className={style.grid}>
 					<div className={style.card}>
 						<img
@@ -18,42 +23,42 @@ const TeamGrid = () => {
 							src="./teampics/IMG_8664.jpg"
 							alt="team member"
 						/>
-                    </div>
+					</div>
 					<div className={style.card}>
 						<img
 							className={style.profilepic}
 							src="./teampics/IMG_8625.jpg"
 							alt="team member"
 						/>
-                    </div>
+					</div>
 					<div className={style.card}>
 						<img
 							className={style.profilepic}
 							src="./teampics/IMG_8692.jpg"
 							alt="team member"
 						/>
-                    </div>
+					</div>
 					<div className={style.card}>
 						<img
 							className={style.profilepic}
 							src="./teampics/IMG_8766.jpg"
 							alt="team member"
 						/>
-                    </div>
+					</div>
 					<div className={style.card}>
 						<img
 							className={style.profilepic}
 							src="./teampics/IMG_8772.jpg"
 							alt="team member"
 						/>
-                    </div>
+					</div>
 					<div className={style.card}>
 						<img
 							className={style.profilepic}
 							src="./teampics/IMG_8710.jpg"
 							alt="team member"
 						/>
-                    </div>
+					</div>
 				</div>
 			</div>
 		</div>
